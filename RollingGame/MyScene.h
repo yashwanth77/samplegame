c3d4bc0ce3d52e57a2f8b65406e0ef428c451bc5
Yashwanth <yashwanth@Yashwanths-Mac-mini.local>
2014-08-22 11:39:23 +0530

    Initial Commit

---
+++ RollingGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  RollingGame
+//
+
+//  Copyright (c) 2014 Yashwanth. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ RollingGame/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RollingGame
+//
+
+//  Copyright (c) 2014 Yashwanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
